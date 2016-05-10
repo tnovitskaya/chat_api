ad1222961b2ad06ff7b783dad2547f3f87b029b0
Tanya <tanyanvt@gmail.com>
2016-05-10 16:03:33 +0300

    Set the routes for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+    # Api definition
+  namespace :api, defaults: { format: :json },
+                              constrains: { subdomain: 'api' }, path: '/' do
+    # We are going to list our resources here
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
