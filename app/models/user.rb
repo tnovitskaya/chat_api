da4aafd55143380e5e301e07431ae84bc1c7edf3
Tanya <tanyanvt@gmail.com>
2016-05-11 20:06:21 +0300

    Add user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 
+  before_create :generate_authentication_token!
+
   validates_uniqueness_of   :username, :case_sensitive => false, :allow_blank => true
   validates_presence_of     :username, on: [:create, :update]
   validates_presence_of     :password, on: :create
   validates_confirmation_of :password, :on=>:create
   validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
+
+  validates :auth_token, uniqueness: true
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ describe User do
   it { should respond_to(:username) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
 
   it { should be_valid }
 
@@ -15,4 +16,19 @@ describe User do
   it { should validate_uniqueness_of(:username) }
   it { should validate_confirmation_of(:password) }
   it { should ensure_length_of(:password) }
+  it { should validate_uniqueness_of(:auth_token)}
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      allow(Devise).to receive(:friendly_token).and_return("token123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "token123"
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "token123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
+  end
 end
