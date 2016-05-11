fb73e440dd161399f0fb93c9ef1356e1fd3daffa
Tanya <tanyanvt@gmail.com>
2016-05-11 19:45:05 +0300

    refactoring tests

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Api::V1::UsersController do
-  #before(:each) { request.headers['Accept'] = "application/vnd.localhost:3000.v1" }
-
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
-      user_response = JSON.parse(response.body, symbolize_names: true)
+      user_response = json_response
       expect(user_response[:username]).to eql @user.username
     end
 
@@ -22,83 +20,82 @@ RSpec.describe Api::V1::UsersController do
     context "when is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:username]).to eql @user_attributes[:username]
       end
 
-      it { should respond_with 201 }
+      it { is_expected.to respond_with 201 }
     end
 
     context "when is not created" do
       before(:each) do
         @invalid_user_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        post :create, { user: @invalid_user_attributes }
       end
 
       it "renders an errors json" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
       it "renders the json errors on why the user could not be created" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:errors][:username]).to include "can't be blank"
       end
 
-      it { should respond_with 400 }
+      it { is_expected.to respond_with 400 }
     end
   end
 
   describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+    end
 
     context "when is successfully updated" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { username: "newmail@example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { username: "newname" } }
       end
 
       it "renders the json representation for the updated user" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:username]).to eql "newmail@example.com"
+        user_response = json_response
+        expect(user_response[:username]).to eql "newname"
       end
 
-      it { should respond_with 200 }
+      it { is_expected.to respond_with 200 }
     end
 
     context "when is not created" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { username: "" } }, format: :json
+        patch :update, { id: @user.id, user: { username: "" } }, format: :json
       end
 
       it "renders an errors json" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whye the user could not be created" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+      it "renders the json errors on why the user could not be created" do
+        user_response = json_response
         expect(user_response[:errors][:username]).to include "can't be blank"
       end
 
-      it { should respond_with 400 }
+      it { is_expected.to respond_with 400 }
     end
   end
 
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
-      delete :destroy, { id: @user.id }, format: :json
+      delete :destroy, { id: @user.id }
     end
 
-    it { should respond_with 204 }
+    it { is_expected.to respond_with 204 }
 
   end
 end
